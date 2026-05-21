ee7eaa5236bdac833ecb64c455ea8387b25e2867
LeHoangViet9 <leviet20051509@gmail.com>
2026-05-21 18:38:55 +0700

    first commit

---
+++ src/main/java/com/hms/HotelManagementSystemSwpApplication.java
@@ -0,0 +1,13 @@
+package com.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystemSwpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementSystemSwpApplication.class, args);
+    }
+
+}
