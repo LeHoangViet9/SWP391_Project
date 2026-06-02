0ba8db23bcacf9a53b5ac0c5ceb1812cd1bf2538
Hoàng <chuvanhoanggago8a2@gmail.com>
2026-06-02 16:01:35 +0700

    update Equipment

--- src/main/java/com/hms/controller/equipment/EquipmentController.java
+++ src/main/java/com/hms/controller/equipment/EquipmentController.java
@@ -1,45 +1,74 @@
 package com.hms.controller.equipment;
+
+import com.hms.common.dto.ApiResponse;
 import com.hms.dto.equipment.request.EquipmentCreateDTO;
 import com.hms.dto.equipment.response.EquipmentResponse;
 import com.hms.service.equipment.EquipmentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/equipments")
+@RequestMapping("/api/v1/equipments")
 @RequiredArgsConstructor
 public class EquipmentController {
     private final EquipmentService equipmentService;
 
-    @PostMapping
-    public EquipmentResponse createEquipment(
-            @RequestBody EquipmentCreateDTO dto) {
-        return equipmentService.createEquipment(dto);
-    }
-
     @GetMapping
-    public List<EquipmentResponse> getEquipments() {
-        return equipmentService.getEquipments();
+    public ResponseEntity<ApiResponse<List<EquipmentResponse>>> findAll() {
+        return new ResponseEntity<>(new ApiResponse<>(
+                true,
+                "equipment.getall.success",
+                equipmentService.getEquipments(),
+                HttpStatus.OK
+        ), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public EquipmentResponse getEquipmentById(
-            @PathVariable Long id) {
-        return equipmentService.findById(id);
+    public ResponseEntity<ApiResponse<EquipmentResponse>> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(new ApiResponse<>(
+                true,
+                "equipment.getbyid.success",
+                equipmentService.findById(id),
+                HttpStatus.OK
+        ), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<EquipmentResponse>> createEquipment(
+            @Valid @RequestBody EquipmentCreateDTO dto) {
+        return new ResponseEntity<>(new ApiResponse<>(
+                true,
+                "equipment.add.success",
+                equipmentService.createEquipment(dto),
+                HttpStatus.CREATED
+        ), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public EquipmentResponse updateEquipment(
-            @PathVariable Long id,
-            @RequestBody EquipmentCreateDTO dto) {
-        return equipmentService.updateEquipment(id, dto);
+    public ResponseEntity<ApiResponse<EquipmentResponse>> updateEquipment(
+            @Valid @RequestBody EquipmentCreateDTO dto,
+            @PathVariable Long id) {
+        return new ResponseEntity<>(new ApiResponse<>(
+                true,
+                "equipment.update.success",
+                equipmentService.updateEquipment(id, dto),
+                HttpStatus.OK
+        ), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteEquipment(
-            @PathVariable Long id) {
+    public ResponseEntity<ApiResponse<Void>> deleteEquipment(@PathVariable Long id) {
         equipmentService.deleteEquipment(id);
+        return new ResponseEntity<>(new ApiResponse<>(
+                true,
+                "equipment.delete.success",
+                null,
+                HttpStatus.NO_CONTENT
+        ), HttpStatus.NO_CONTENT);
     }
 }
