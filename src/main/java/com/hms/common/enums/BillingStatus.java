ca182b1c12fb50ef203eaeaa72d3782c5757e831
tu <tientutqp@gmail.com>
2026-06-04 00:44:33 +0700

    enums for billing

---
+++ src/main/java/com/hms/common/enums/BillingStatus.java
@@ -0,0 +1,5 @@
+package com.hms.common.enums;
+
+public enum BillingStatus {
+    PENDING,COMPLETED,OVERDUE,CANCELLED,PARTIAL_PAID
+}
---
+++ src/main/java/com/hms/common/enums/PaymentMethod.java
@@ -0,0 +1,11 @@
+package com.hms.common.enums;
+
+public enum PaymentMethod {
+    CASH,
+    CARD,
+    BANK_TRANSFER,
+    E_WALLET,
+    CHECK
+}
+
+
