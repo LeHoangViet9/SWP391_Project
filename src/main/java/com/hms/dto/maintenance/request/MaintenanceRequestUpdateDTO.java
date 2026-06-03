087fadfd2e2a49431189c4db101e7969a64fd556
Hoàng <chuvanhoanggago8a2@gmail.com>
2026-06-03 23:37:11 +0700

    Maintenance_enums,request

--- src/main/java/com/hms/dto/maintenance/request/MaintenanceRequestUpdateDTO.java
+++ src/main/java/com/hms/dto/maintenance/request/MaintenanceRequestUpdateDTO.java
@@ -1,5 +1,27 @@
 package com.hms.dto.maintenance.request;
 
+import com.hms.common.enums.MaintenanceSeverity;
+import com.hms.common.enums.MaintenanceStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MaintenanceRequestUpdateDTO {
+
+        private String diagnosis;
+
+        private String repairResult;
+
+        @NotNull(message = "{maintenance.severity.notnull}")
+        private MaintenanceSeverity severity;
+
+        @NotNull(message = "{maintenance.status.notnull}")
+        private MaintenanceStatus status;
+
+        private Long assignedTo;
+
 
 }
