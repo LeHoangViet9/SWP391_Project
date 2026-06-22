53c1b42d261631104a67d7d8e9c8c26d7baaa1ed
nguoibian863 <nguoibian863@gmail.com>
2026-06-23 06:20:37 +0700

    refactor(booking): move EnableScheduling annotation to separate SchedulingConfig class

--- src/main/java/com/hms/HotelManagementSystemSwpApplication.java
+++ src/main/java/com/hms/HotelManagementSystemSwpApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@org.springframework.scheduling.annotation.EnableScheduling
 public class HotelManagementSystemSwpApplication {
 
     public static void main(String[] args) {
